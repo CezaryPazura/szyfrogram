9bf75f21b7a8d6feb33884c4f360badbc6111b4c
student <student@ckziu.jaworzno.pl>
2022-11-15 14:06:15 +0100

    Initial commit

---
+++ qwe.py
@@ -0,0 +1,30 @@
+class Ex1:
+    def __init__(self, plaintext):
+        self.text = plaintext
+
+    def encrypt(self, cryptogram):
+        tabofconsonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "r", "s", "t", "w", "x", "z"]
+        consonant = ""
+        for x in range(len(self.text)):
+            a = x
+            for y in range(len(tabofconsonants)):
+                if self.text[a] == tabofconsonants[y]:
+                    cryptogram[a] = consonant
+                    consonant = self.text[x]
+            for z in range(len(tabofconsonants)):
+                if self.text[0] == tabofconsonants[z]:
+                    cryptogram[0] = consonant
+                if self.text[1] == tabofconsonants[z]:
+                    cryptogram[1] = consonant
+        return cryptogram
+
+
+plaintext = input("Napisz slowo które chcesz zmienić: ")
+tab = list(plaintext.lower())
+tab2 = list(plaintext.lower())
+
+a1 = Ex1(tab)
+print(a1.encrypt(tab2))
+
+
+
